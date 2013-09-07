10dec0b6d4c67197896d66bf1f3dd39278db28d1
Ryan Baker <ryanfbaker@gmail.com>
2013-09-07 14:42:33 -0700

    Updated for 2013-2014
    Besides adding new student info, modified the code to:
    - process genders
    - not pick the same person twice
    - display more info in the reveal modal

--- app.py
+++ app.py
@@ -2,20 +2,28 @@ import random
 from flask import Flask, render_template
 app = Flask(__name__)
 
-file = open('studentdata', 'r')
-lines = file.readlines()
-global lines
-file.close()
+app.debug = True
 
+@app.route("/favicon.ico")
+def favicon():
+    return app.send_static_file("favicon.ico")
+
+file = open('studentdata', 'r')
+global lines
+lines = file.readlines()
+
+file.close()
+ 
 @app.route("/")
 def index():
     numberOfStudents = 3
     students = chooseStudents(numberOfStudents)
     
     name = []
-    handle = []
+    photo = []
     year = []
     focus = []
+    gender = []
     featured = []
 
     studentIndex = 0
@@ -25,15 +33,18 @@ def index():
 
         if (studentIndex == 0):
             featuredName = cols[0]
-            featuredFocus = cols[4]
+            featuredPhoto = cols[1]
+            featuredFocus = cols[3]
+            featuredYear = cols[2]
             featured.append('featured')
         else:
             featured.append('notfeatured')
 
         name.append(cols[0])
-        handle.append(cols[1])
+        photo.append(cols[1])
         year.append(cols[2])
-        focus.append(cols[4])
+        focus.append(cols[3])
+        gender.append(cols[4])
         studentIndex += 1
 
     order = [0, 1, 2]
@@ -41,14 +52,21 @@ def index():
 
     # for the record, this is a dump way of passing data to the template.. I didn't realize that Flask templates
     # support logic for loops and multiple data structures. this and associated code above should be simplified
-    return render_template('index.html', featuredName=featuredName, featuredFocus=featuredFocus, name1=name[order[0]], handle1=handle[order[0]], year1=year[order[0]], focus1=focus[order[0]], featured1=featured[order[0]], name2=name[order[1]], handle2=handle[order[1]], year2=year[order[1]], focus2=focus[order[1]], featured2=featured[order[1]], name3=name[order[2]], handle3=handle[order[2]], year3=year[order[2]], focus3=focus[order[2]], featured3=featured[order[2]])
+    return render_template('index.html', featuredName=featuredName, featuredFocus=featuredFocus, featuredYear=featuredYear, featuredPhoto=featuredPhoto, name1=name[order[0]], photo1=photo[order[0]], year1=year[order[0]], focus1=focus[order[0]], featured1=featured[order[0]], name2=name[order[1]], photo2=photo[order[1]], year2=year[order[1]], focus2=focus[order[1]], featured2=featured[order[1]], name3=name[order[2]], photo3=photo[order[2]], year3=year[order[2]], focus3=focus[order[2]], featured3=featured[order[2]])
 
 def chooseStudents(num=3):
     students = []
+    genders = ['M', 'F']
+    thisgender = random.choice(genders)
     while (len(set(students)) < num):
         randomid = random.randrange(0, len(lines))
-        students.append(randomid)
+        student = lines[randomid] 
+        cols = student.split('\t')
+        if cols[4].strip() == thisgender and cols[1] != "placeholder.jpg" and randomid not in students:
+            students.append(randomid)
+        
     return students
+    
 
 if __name__ == "__main__":
     app.run()
---
+++ getstudents.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import urllib
+import urllib2
+import urlparse
+import re, string
+from bs4 import BeautifulSoup
+
+def RequestResponse(url):
+    '''
+    This method makes an HTTP request over the network (just like a web browser)
+    and returns the contents of the requested url.
+    '''
+    try:
+        response = urllib2.urlopen(url)
+        return response
+    except urllib2.URLError as err:
+        print("Error opening url {} .\nError is: {}".format(url, err))
+        return False
+
+
+def getNames(url, output=True):
+    
+    response = RequestResponse(url)
+    if not response: return
+    soup = BeautifulSoup(response.read())
+    
+    students = []
+    student_list = soup.find_all("table", "person-teaser")
+    
+    for student_entry in student_list:
+
+        student_img = student_entry.img['src'].split('/')[-1]
+        
+        ''' Uncomment this to download the images. '''
+        #urllib.urlretrieve("http://www.ischool.berkeley.edu/files/imagecache/profile-pic/" + str(student_img), "static/profilephotos/" + student_img)
+        
+        student_name = student_entry.find("div", "title").text.encode('ascii', 'replace')
+        student_year = student_entry.find("div", "field-field-person-degree-year").text
+        try:
+            student_focus = student_entry.find("div", "field-field-person-focus").text[6:]
+        except:
+            student_focus = ""
+            
+        #print('{}\t{}\t{}\t{}'.format(student_name.strip(), student_img, student_year.strip(), student_focus.strip()))
+        students.append('{}\t{}\t{}\t{}'.format(student_name.strip(), student_img, student_year.strip(), student_focus.strip()))
+    
+    
+    ''' write the names and info to a tab-delimited text file. Note that it won't pick up the genders - you have to manually enter those. Open the file in excel and make a new column after focus and fill with either M or F.'''
+    outputfile = open('studentdata_test', "a")
+    for i in students:
+        outputfile.write(i)
+        outputfile.write("\n")
+    outputfile.close()
+    print "Student names saved."      
+        
+if __name__ == '__main__':
+
+    getNames('http://www.ischool.berkeley.edu/people/students/mims')
+    getNames('http://www.ischool.berkeley.edu/people/students/phd')
